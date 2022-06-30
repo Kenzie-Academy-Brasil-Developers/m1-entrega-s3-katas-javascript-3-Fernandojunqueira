4b28da7012c5b44c5439b45a4ac38fd3b0e2ceff
Fernandojunqueira <fernandojunqueira12@gmail.com>
2022-06-30 15:17:27 -0300

    Entrega sexta

--- katas3.js
+++ katas3.js
@@ -1,76 +1,198 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
 function kata1() {
-    // implemente o código do kata 1 aqui
+    let arr = []
+    for(i=1;i<=25;i++){
+        arr.push(i)
+    }
+    return arr
 }
+console.log(kata1())
 
 function kata2() {
-    // implemente o código do kata 2 aqui
+    let arr = []
+    for(i=25;i>=1;i--){
+        arr.push(i)
+    }
+    return arr
 }
+console.log(kata2())
 
 function kata3() {
-    // implemente o código do kata 3 aqui
+    let arr = []
+    for(i=-1;i>=-25;i--){
+        arr.push(i)
+    }
+    return arr
 }
+console.log(kata3())
 
 function kata4() {
-    // implemente o código do kata 4 aqui
+    let arr = []
+    for(i=-25;i<=-1;i++){
+        arr.push(i)
+    }
+    return arr
 }
+console.log(kata4())
 
 function kata5() {
-    // implemente o código do kata 5 aqui
+    let arr = []
+    for(i=25;i>=-25;i--){
+        arr.push(i)
+    }
+    return arr
 }
+console.log(kata5())
+
 
 function kata6() {
-    // implemente o código do kata 6 aqui
+    let arr = []
+    for(i=1;i<=100;i++){
+        if(i%3 === 0){
+            arr.push(i)
+        }
+    }
+    return arr
 }
+console.log(kata6())
+
 
 function kata7() {
-    // implemente o código do kata 7 aqui
+    let arr = []
+    for(i=1;i<=100;i++){
+        if(i%7 === 0){
+            arr.push(i)
+        }
+    }
+    return arr
 }
+console.log(kata7())
 
 function kata8() {
-    // implemente o código do kata 8 aqui
+    let arr = []
+    for(i=100;i>1;i--){
+        if(i%3 === 0 || i%7 === 0 ){
+            arr.push(i)
+        }
+    }
+    return arr
 }
+console.log(kata8())
 
 function kata9() {
-    // implemente o código do kata 9 aqui
+    let arr = []
+    for(i=1;i<=100;i++){
+        if(i%2 !== 0 && i%5 === 0 ){
+            arr.push(i)
+        }
+    }
+    return arr
 }
+console.log(kata9())
 
 function kata10() {
-    // implemente o código do kata 10 aqui
+    let arr = []
+    for(i=0;i<sampleArray.length;i++){
+        
+        arr.push(sampleArray[i])
+        
+    }
+    return arr
 }
+console.log(kata10())
 
 function kata11() {
-    // implemente o código do kata 11 aqui
+    let arr = []
+    for(i=0;i<sampleArray.length;i++){
+       if(sampleArray[i]%2 === 0) {
+        arr.push(sampleArray[i])
+       }
+    }
+    return arr
 }
+console.log(kata11())
 
 function kata12() {
-    // implemente o código do kata 12 aqui
+    let arr = []
+    for(i=0;i<sampleArray.length;i++){
+       if(sampleArray[i]%2 !== 0) {
+        arr.push(sampleArray[i] )
+       }
+    }
+    return arr
 }
+console.log(kata12())
 
 function kata13() {
-    // implemente o código do kata 13 aqui
+    let arr = []
+    for(i=0;i<sampleArray.length;i++){
+       if(sampleArray[i]%8 === 0) {
+        arr.push(sampleArray[i] )
+       }
+    }
+    return arr
 }
+console.log(kata13())
 
 function kata14() {
-    // implemente o código do kata 14 aqui
+    let arr = []
+    for(i=0;i<sampleArray.length;i++){
+
+        arr.push(sampleArray[i]*sampleArray[i])
+       
+    }
+    return arr
 }
+console.log(kata14())
 
 function kata15() {
-    // implemente o código do kata 15 aqui
+    let sum = 0
+    for(i=0;i<=20;i++){
+
+        sum+= i
+       
+    }
+    return sum
 }
+console.log(kata15())
 
 function kata16() {
-    // implemente o código do kata 16 aqui
+    let sum = 0
+    for(i=0;i<sampleArray.length;i++){
+
+       sum+= sampleArray[i]
+       
+    }
+    return sum
 }
+console.log(kata16())
 
 function kata17() {
-    // implemente o código do kata 17 aqui
+    let menorElemento = +Infinity
+    for(i=0;i<sampleArray.length;i++){
+
+        if(sampleArray[i]<menorElemento){
+            menorElemento = sampleArray[i]
+        }
+       
+    }
+    return menorElemento
 }
+console.log(kata17())
 
 function kata18() {
-    // implemente o código do kata 18 aqui
+    let maiorElemento = -Infinity
+    for(i=0;i<sampleArray.length;i++){
+
+        if(sampleArray[i]>maiorElemento){
+            maiorElemento = sampleArray[i]
+        }
+       
+    }
+    return maiorElemento
 }
+console.log(kata18())
 
 /**
  * Daqui em diante são os bônus, por sua conta e risco
